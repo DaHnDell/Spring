aacb3af1f54a7dbc390984118676adfcb8afc5aa
DahnDell <khanman1208@gmail.com>
2024-12-11 15:35:40 +0900

    PostTest mod

--- di/src/main/java/com/kcanmin/di/test/PostTest.java
+++ di/src/main/java/com/kcanmin/di/test/PostTest.java
@@ -1,21 +1,22 @@
-// package com.kcanmin.di.test;
+package com.kcanmin.di.test;
 
-// import org.apache.catalina.core.ApplicationContext;
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-// import com.kcanmin.di.service.PostService;
-// import com.kcanmin.di.service.PostServiceImplNormal;
+import com.kcanmin.di.service.PostService;
+import com.kcanmin.di.service.PostServiceImplNormal;
 
-// import lombok.extern.log4j.Log4j2;
+import lombok.extern.log4j.Log4j2;
 
 // @SpringBootApplication
-// @Log4j2
-// public class PostTest {
-//     public static void main(String[] args) {
-//         SpringApplication application = new SpringApplication(PostTest.class);
-//         ApplicationContext ctx = application.run(args);
-//         PostService service = ctx.getBean(PostServiceImplNormal.class);
-//         service.list().forEach(log::info);
-//     }
-// }
+@Log4j2
+public class PostTest {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(PostTest.class); // 부트 기반으로 파일 가져오기
+        ConfigurableApplicationContext ctx = SpringApplication.run(PostTest.class, args); // 어쨌든 컨텍스트 기반으로 동작함. 
+        PostService service = ctx.getBean(PostService.class); // required 파일 받으면 그냥 OBJ로 정의됨.    
+        service.list().forEach(log::info);      
+    }
+}
