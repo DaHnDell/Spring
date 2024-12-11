c37e3c3556bc7deb5780fe0c482cd1266208024a
DahnDell <khanman1208@gmail.com>
2024-12-11 12:39:27 +0900

    DiApplication Annotation add

--- di/src/main/java/com/kcanmin/di/DiApplication.java
+++ di/src/main/java/com/kcanmin/di/DiApplication.java
@@ -2,7 +2,11 @@ package com.kcanmin.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import lombok.Data;
+
+@Data
 @SpringBootApplication
 public class DiApplication {
 
@@ -10,4 +14,6 @@ public class DiApplication {
 		SpringApplication.run(DiApplication.class, args);
 	}
 
+	// @Bean 참조 자료형이라면 뭐든 등록이 가능. 그러니까 꼭 객체가 아니어도 반환이 무언가 있다면 가능. 인스턴스 자체를 반환하거나. 그래서 기본 자료형은 Bean 의 대상이 될 수 없음.
+	
 }
