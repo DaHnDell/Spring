a47d00f95145ebe9ac042b0be65312f24740b98c
DahnDell <khanman1208@gmail.com>
2024-12-17 15:08:38 +0900

    webconfig add / commonController add

--- springpost/member_post/src/main/java/com/kcanmin/member_post/config/WebConfig.java
+++ springpost/member_post/src/main/java/com/kcanmin/member_post/config/WebConfig.java
@@ -27,4 +27,6 @@ public class WebConfig implements WebMvcConfigurer{
   }
   // 웹리스너 어노테이션으로 하는 것이 아니라 빈의 라이프사이클에게 선물해주는 것.
   // 원칙에 위배되지 않는 pojo 문법으로 익명클래스를 가져다주는 형태로 하여도 Spring이 알아서 작동함.
+  // contextpath 리스너를 살리는 형태로도 가능하긴 하지만, 이에 따른 추가적인 작업이 더욱 더 복잡함.
+  // 또한 모듈화, 응집성 측면에서도 webconfig 에 정의해주는 것이 더 타당하다. 
 }
--- springpost/member_post/src/main/java/com/kcanmin/member_post/controller/CommonController.java
+++ springpost/member_post/src/main/java/com/kcanmin/member_post/controller/CommonController.java
@@ -1,8 +1,10 @@
 package com.kcanmin.member_post.controller;
 
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
@@ -13,5 +15,13 @@ public class CommonController {
   public String index(){
     log.info("index Started");
     return "common/index";
+  }
+
+    
+  @RequestMapping(value = "msg")
+  public String msg(@ModelAttribute("msg") String msg, @Nullable String url) {
+    log.info(msg);
+    log.info(url);
+    return "common/msg";
   }
 }
