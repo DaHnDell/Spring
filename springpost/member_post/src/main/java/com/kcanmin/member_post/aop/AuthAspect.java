522261ab6a71d155260fb3856ee675f45c4c053f
DahnDell <khanman1208@gmail.com>
2024-12-18 18:14:43 +0900

    AuthAspect Issue Loaded / end

--- springpost/member_post/src/main/java/com/kcanmin/member_post/aop/AuthAspect.java
+++ springpost/member_post/src/main/java/com/kcanmin/member_post/aop/AuthAspect.java
@@ -1,16 +1,32 @@
 package com.kcanmin.member_post.aop;
 
+// import java.io.IOException;
+// import java.net.URLEncoder;
+import java.util.Arrays;
+
+// import org.aspectj.lang.JoinPoint;
+// // import org.aspectj.lang.ProceedingJoinPoint;
+// import org.aspectj.lang.annotation.Aspect;
+// import org.aspectj.lang.annotation.Before;
+// import org.springframework.stereotype.Component;
+
+import com.kcanmin.member_post.vo.Member;
+// import com.kcanmin.member_post.vo.Post;
+
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.http.HttpSession;
+// import lombok.AllArgsConstructor;
+// import lombok.extern.log4j.Log4j2;
+
 import java.io.IOException;
 import java.net.URLEncoder;
 
 import org.aspectj.lang.JoinPoint;
-// import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-import com.kcanmin.member_post.vo.Member;
-import com.kcanmin.member_post.vo.Post;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -23,25 +39,45 @@ import lombok.extern.log4j.Log4j2;
 @AllArgsConstructor
 @Log4j2
 public class AuthAspect {
-  private HttpSession httpSession;
+  private HttpSession session;
   private HttpServletResponse resp;
   private HttpServletRequest req;
 
-  @Before("@annotation(com.kcanmin.member_post.aop.MyPost)")
-  public void myPost(JoinPoint joinPoint, Post post){
-    Object o = httpSession.getAttribute("member");
-    if(o==null || !((Member)o).getId().equals(post.getWriter())){
-      throw new RuntimeException("not your post; invalid approach");
-    }
+   @Before("@annotation(com.kcanmin.member_post.aop.MyPost)")
+  public void myPost(JoinPoint joinPoint, MyPost myPost){
+    Object o = session.getAttribute("member");
+    String id = ((Member) o).getId();
+
+    Object[] args = joinPoint.getArgs();
+    String writeParam = myPost.value();
+    log.info(Arrays.toString(args));
+    log.info(id);
+    log.info(writeParam);
+
+    // if(o == null || ((Member)o).getId().equals(post.getWriter())) {
+    //   throw new RuntimeException("본인 게시글 아님");
+    // }
   }
+
+  // @Before("write()")
   @Before("@annotation(com.kcanmin.member_post.aop.SigninCheck)")
-  public void SigninCheck(JoinPoint joinPoint) throws IOException{
-    // log.info(req.getRequestURI());
-    // log.info(req.getRequestURL());
-    // log.info(req.getQueryString());
-    if(httpSession.getAttribute("member") == null){
-      String url = "/member/signin?url=" + URLEncoder.encode(req.getRequestURI() +  "?" + req.getQueryString(), "utf-8");
-      resp.sendRedirect("/msg?msg=" + URLEncoder.encode("plz, Log in first", "utf-8")+"&url="+ url);
+  public void signinCheck(JoinPoint jp) throws IOException {
+    log.info(req.getRequestURL());
+    log.info(req.getRequestURI());
+    log.info(req.getQueryString());
+    log.info(session.getAttribute("member"));
+    // if(session.getAttribute("member") == null) {
+    //   log.error(url);
+    //   resp.sendRedirect("/msg?msg=" + URLEncoder.encode("로그인이 필요한 서비스입니다.", "utf-8") + "&url=" + url);
+    //   return;
+    // }
+    if (session.getAttribute("member") == null ){
+      String url = "/member/signin?url=" + URLEncoder.encode(req.getRequestURI() + "?" + req.getQueryString(), "utf-8");
+      resp.sendRedirect("/msg?msg=" + URLEncoder.encode("로그인이 필요한 페이지 입니다.", "utf-8")+ "&url="+url);
     }
-  }
+    // else{
+    //   resp.sendRedirect(req.getRequestURL() + req.getQueryString());
+    // }
+    // }
+  } 
 }
