c6b1e8b49521bda0770a1d63e95e934737673ca2
DahnDell <khanman1208@gmail.com>
2024-12-23 11:10:24 +0900

    1교시 수업내용 커밋

--- todo/todo/src/main/java/com/kcanmin/todo/controller/TodoController.java
+++ todo/todo/src/main/java/com/kcanmin/todo/controller/TodoController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.kcanmin.todo.dto.TodoListDto;
 import com.kcanmin.todo.dto.TodoWriteDto;
 import com.kcanmin.todo.service.TodoService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -33,6 +38,15 @@ public class TodoController {
     return "redirect:/todos";
   }
   
+  @RequestMapping("todos/remove")
+  public String remove(Long id) {
+      // toDoService.remove(dto.getId()); 작업 순서를 기억해라,, 로그부터 찍어 볼 것
+      log.info(id);
+      toDoService.remove(id);
+      return "redirect:/todos";
+  }
+  
+
   // @GetMapping("list")
   // public String getMethodName(@RequestParam String param) {
   //     return new String();
--- todo/todo/src/main/java/com/kcanmin/todo/service/TodoService.java
+++ todo/todo/src/main/java/com/kcanmin/todo/service/TodoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.kcanmin.todo.domain.TodoEntity;
 import com.kcanmin.todo.domain.TodoRepository;
 import com.kcanmin.todo.dto.TodoListDto;
 import com.kcanmin.todo.dto.TodoWriteDto;
@@ -19,11 +20,17 @@ public class TodoService {
   public List<TodoListDto> list() {
     return repository.findAll().stream().map(TodoListDto::new).toList();
   }
-
+  
+  // 등록
   public void write(TodoWriteDto dto) {
     repository.save(dto.toEntity());
   }
-  // 등록
 
   // 삭제
+  public void remove(Long id) {
+    // repository.delete(TodoEntity.builder().id(id).build()); //엔티티 호출하는 방식,
+    repository.deleteById(id); //아이디 방식으로 전체삭제
+    // 가장 쉬운 방법은 
+  }
+
 }
