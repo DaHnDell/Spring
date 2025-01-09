0ab6d10f8fcedc47c72028d05698311846d1e0ed
DahnDell <khanman1208@gmail.com>
2025-01-09 18:26:15 +0900

    SecurityConfig mod

--- club/src/main/java/com/kcanmin/club/config/SecurityConfig.java
+++ club/src/main/java/com/kcanmin/club/config/SecurityConfig.java
@@ -2,27 +2,35 @@ package com.kcanmin.club.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Configuration
 public class SecurityConfig {
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
       http
-          .csrf().disable() // CSRF 비활성화 (필요에 따라 활성화)
+          .csrf(csrf -> csrf.disable()) // CSRF 비활성화 (필요에 따라 활성화)
           .authorizeHttpRequests(auth -> auth
-              // .requestMatchers("//").permitAll() // `/public/` 경로는 인증 없이 접근 가능
+              .requestMatchers("/sample/all").permitAll()
+              .requestMatchers("/sample/member").hasRole("USER")
+              .requestMatchers("/sample/admin").hasRole("ADMIN") 
               .anyRequest().authenticated() // 나머지는 인증 필요
           )
-          .formLogin(); // 기본 로그인 폼 활성화
+          .formLogin(form -> form.permitAll()) // 기본 로그인 폼 활성화
+          .logout(form -> form.logoutUrl("/member/signout"))
+          .oauth2Login(Customizer.withDefaults());
       return http.build();
   }
 }
