01f44e5bf17e3a46d57c2ad3444ebfd5310ffc48
DahnDell <khanman1208@gmail.com>
2025-01-10 11:27:10 +0900

    Add new Social Login member to DB automatically;

--- club/src/main/java/com/kcanmin/club/security/dto/AuthMemberDTO.java
+++ club/src/main/java/com/kcanmin/club/security/dto/AuthMemberDTO.java
@@ -1,9 +1,11 @@
 package com.kcanmin.club.security.dto;
 
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,18 +16,35 @@ import lombok.extern.log4j.Log4j2;
 @Getter
 @Setter
 @ToString
-public class AuthMemberDTO extends User{
+public class AuthMemberDTO extends User implements OAuth2User{
 
   private Long mno;
   private String email;
   private String name;
   private Boolean fromSocial;
+  private Map<String, Object> attr;
 
+  // OAuth2 호출 생성자
+  public AuthMemberDTO(String username, String password, Long mno, Boolean fromSocial, String name, Collection<? extends GrantedAuthority> authorities, Map<String, Object> attr){
+    super(username, password, authorities);
+    this.mno = mno;
+    this.email = username;
+    this.fromSocial = fromSocial;
+    this.name = name;
+    this.attr = attr;
+  }
+
+  // Security 자체 로그인 호출 생성자
   public AuthMemberDTO(String username, String password, Long mno, Boolean fromSocial, String name, Collection<? extends GrantedAuthority> authorities){
     super(username, password, authorities);
     this.mno = mno;
     this.email = username;
     this.fromSocial = fromSocial;
     this.name = name;
+  }
+
+  @Override
+  public Map<String, Object> getAttributes(){
+    return this.attr;
   }
 }
--- club/src/main/java/com/kcanmin/club/service/Oauth2UserDetailsService.java
+++ club/src/main/java/com/kcanmin/club/service/Oauth2UserDetailsService.java
@@ -3,6 +3,7 @@ package com.kcanmin.club.service;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -13,7 +14,9 @@ import org.springframework.stereotype.Service;
 import com.kcanmin.club.entity.Member;
 import com.kcanmin.club.entity.MemberRole;
 import com.kcanmin.club.repository.MemberRepository;
+import com.kcanmin.club.security.dto.AuthMemberDTO;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -30,33 +33,38 @@ public class Oauth2UserDetailsService extends DefaultOAuth2UserService{
    * @return return OAuth2User;
    */
   @Override
+  @Transactional
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
     log.info(userRequest);
     String clientName = userRequest.getClientRegistration().getClientName();
     Map<?, ?> params = userRequest.getAdditionalParameters();
     OAuth2User auth2User = super.loadUser(userRequest);
-    
+
     log.info("클라이언트 이름 : : : " + clientName);
     log.info("파라미터 : : : " + params);
 
     String authedUserEmail = null;
     auth2User.getAttributes().forEach((k, v) -> {log.info("이것은 만능열쇠 키~ : : : " + k); log.info("밸류 밸류 뉴 밸류 : : : " + v);});
-
-    if(clientName.equals("google")){
+    if(clientName.equalsIgnoreCase("google")){
       log.info(": : : Email : : : =============================");
-      authedUserEmail = auth2User.getAttributes().get("email").toString();
+      authedUserEmail = auth2User.getAttribute("email");
       log.info("===============================================");
     }
 
-    return auth2User;
+    Member member = saveSocialMember(authedUserEmail);
+    AuthMemberDTO authMemberDTO = new AuthMemberDTO(member.getEmail(), member.getPassword(), member.getMno(), 
+    member.getFromSocial(), member.getName(),member.getRoleSet().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).toList(), auth2User.getAttributes());
+
+    return authMemberDTO;
   }
-  
 
   /**
    * @param email
    * @return member || null
    */
+  @Transactional
   private Member saveSocialMember(String email){
+    log.info(email);
     Member member = repository.findByEmailAndFromSocial(email, true);
     if(member != null){
       return member;
@@ -65,7 +73,7 @@ public class Oauth2UserDetailsService extends DefaultOAuth2UserService{
     Member member2 = Member
       .builder()
       .email(email)
-      .password("1234")
+      .password(encoder.encode("1234"))
       .fromSocial(true)
       .build();
     member2.addMemberRole(MemberRole.USER);
@@ -73,5 +81,4 @@ public class Oauth2UserDetailsService extends DefaultOAuth2UserService{
 
     return member2;
   }
-
 }
