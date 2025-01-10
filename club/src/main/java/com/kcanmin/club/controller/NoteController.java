bb5c668dca79dccce283ba5e6fdc7cb1f14d4488
DahnDell <khanman1208@gmail.com>
2025-01-10 18:22:27 +0900

    NoteController / SampleController add

--- club/src/main/java/com/kcanmin/club/controller/NoteController.java
+++ club/src/main/java/com/kcanmin/club/controller/NoteController.java
@@ -8,21 +8,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.kcanmin.club.entity.dto.NoteDTO;
 import com.kcanmin.club.service.NoteService;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
 @Log4j2
--- club/src/main/java/com/kcanmin/club/controller/SampleController.java
+++ club/src/main/java/com/kcanmin/club/controller/SampleController.java
@@ -6,12 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import lombok.extern.log4j.Log4j2;
 import com.kcanmin.club.security.dto.AuthMemberDTO;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @Controller
 @Log4j2
