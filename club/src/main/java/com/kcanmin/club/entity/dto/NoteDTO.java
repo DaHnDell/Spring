245a54182bff4d0fa2fc20cf8ad84239fa73cddd
DahnDell <khanman1208@gmail.com>
2025-01-16 11:36:41 +0900

    noteDto add

--- club/src/main/java/com/kcanmin/club/entity/dto/NoteDTO.java
+++ club/src/main/java/com/kcanmin/club/entity/dto/NoteDTO.java
@@ -1,11 +1,14 @@
 package com.kcanmin.club.entity.dto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Builder.Default;
 
 @Data
 @AllArgsConstructor
@@ -18,4 +21,7 @@ public class NoteDTO {
   private String writerEmail;
   private LocalDateTime regDate, modDate;
   private Long mno;
+
+  @Default
+  private List<AttachDTO> attachDtos = new ArrayList<>();
 }
